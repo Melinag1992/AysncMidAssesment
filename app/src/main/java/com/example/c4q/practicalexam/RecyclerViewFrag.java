1f430efc40be7ea85ae224eb4093296aaf885190
C4Q <c4q@C4Qs-MacBook-Pro-5.local>
2018-01-16 19:04:45 -0500

    loop fixed

--- app/src/main/java/com/example/c4q/practicalexam/RecyclerViewFrag.java
+++ app/src/main/java/com/example/c4q/practicalexam/RecyclerViewFrag.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.example.c4q.practicalexam.controller.ModelAdapter;
@@ -30,7 +31,6 @@ public class RecyclerViewFrag extends Fragment {
     private String username;
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_layout, container, false);
@@ -46,21 +46,33 @@ public class RecyclerViewFrag extends Fragment {
 
         return v;
     }
-    public void setEmail(){
+
+    public void setEmail() {
         Bundle bundle = getActivity().getIntent().getExtras();
         username = bundle.get("username").toString();
         emailDisplayed.setText(username);
     }
-     public void setRecyclerView(){
-         List<Model> modelList = new ArrayList<>();
-         for(int i =0;i<=1000000;i++){
-             modelList.add(new Model(i));
-         }
 
-         ModelAdapter modelAdapter = new ModelAdapter(modelList);
-         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(v.getContext().getApplicationContext(),LinearLayoutManager.VERTICAL,false);
-         recyclerView.setAdapter(modelAdapter);
-         recyclerView.setLayoutManager(linearLayoutManager);
-     }
+    public void setRecyclerView() {
+
+        List<Model> modelList = new ArrayList<>();
+
+        modelList.add(new Model(0));
+        modelList.add(new Model(1));
+        modelList.add(new Model(2));
+        modelList.add(new Model(3));
+        modelList.add(new Model(4));
+        modelList.add(new Model(5));
+        modelList.add(new Model(6));
+        modelList.add(new Model(7));
+        modelList.add(new Model(8));
+        modelList.add(new Model(9));
+        modelList.add(new Model(10));
+
+        ModelAdapter modelAdapter = new ModelAdapter(modelList);
+        recyclerView.setAdapter(modelAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(v.getContext().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
 
 }
